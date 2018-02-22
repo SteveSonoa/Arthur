eb1b1d857ba694ee5eac0d43d547ff5900bc89a3
Steve Marshall <stevesonoa@gmail.com>
2018-02-22 16:22:31 -0700

    Manually installed mysql2 as requested by sequelize. Added passport requirements to server.js. Updated sequelize config to point to correct directory.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Require the necessary packages
 var express = require('express');
 var bodyParser = require('body-parser');
+var passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
 
 // Sync to the DB before starting the server
 var db = require("./models");
@@ -8,6 +9,12 @@ db.sequelize.sync();
 
 var PORT = process.env.PORT || 3000;
 var app = express();
+
+// Setup Passport session requirements
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -31,6 +38,12 @@ app.post('/login', passport.authenticate('local'), function(req, res) {
 	// `req.user` contains the authenticated user.
 	res.redirect('/users/' + req.user.username);
 });
+
+app.post('/login', passport.authenticate('local', {
+	successRedirect: '/',
+	failureRedirect: '/login',
+	failureFlash: true
+}));
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
