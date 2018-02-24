ebb58b76e6f43016feb507bc9319f8005efc64ec
Steve Marshall <stevesonoa@gmail.com>
2018-02-24 15:58:55 -0700

    playing with linkedin functionality.

--- linkedin.js
+++ linkedin.js
@@ -4,8 +4,6 @@ const bodyParser = require("body-parser");
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const logger = require("morgan");
-const Linkedin = require('node-linkedin')('86iqdrrwi4ttfs', 'Mi8cFFT1VKvypzTu', 'https://arthur-brief.herokuapp.com/');
-
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -37,27 +35,10 @@ app.use(session({
   // cookie: { secure: true}  // Only use if using HTTPS
 }));
 
+
+
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Initialize LinkedIn Authorization
-app.get('/', function(req, res) {
-    // This will ask for permisssions etc and redirect to callback url. 
-    // Linkedin.auth.authorize(res, scope);
-
-    Linkedin.auth.getAccessToken(res, req.query.code, req.query.state, function(err, results) {
-        if ( err )
-            return console.error(err);
- 
-        /**
-         * Results have something like:
-         * {"expires_in":5184000,"access_token":". . . ."}
-         */
- 
-        console.log(results);
-        return res.end();
-    });
-});
 
 // Routes
 require('./routes/routes.js')(app);
