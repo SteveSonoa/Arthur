6f5fcb80a7063b5e457453d10231e70a011d9a00
boginis1 <hollyzoba@gmail.com>
2018-03-02 16:45:51 -0700

    handles log in click

---
+++ public/login.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	// call the login api on sign in
+	$("#signInButton").on("click", function(event){
+		event.preventDefault();
+
+		var email = $("#inputEmail").val();
+		var password = $("#inputPassword").val();
+		
+		$.ajax({
+			url: APIPath + "/api/login",
+			data: {
+				username:email,
+				password:password
+			},
+			method:"post"
+		}).then(function(res){
+			if (res.status !== 200){
+				let errorMessage = res.data.msg;
+				$("#errorMessage").removeClass("hide").text(errorMessage);
+			}
+		})
+	})
+
+})
