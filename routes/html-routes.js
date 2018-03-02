96e5a322f5aebce63a6b75c874ee6be81b661efa
Steve Marshall <stevesonoa@gmail.com>
2018-03-01 21:17:31 -0700

    Updated html-routes to only include 1 set of routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,67 +6,62 @@ const path = require("path");
 module.exports = function(app) {
 
     // Visit home page
-    app.get('/', function(req, res) {
-        // If user is logged in, visit the main page
-        // If user is not logged in, visit splash page
-        res.sendFile(path.join(__dirname, "../public/login.html"));
+    // app.get('/', function(req, res) {
+    //     // If user is logged in, visit the main page
+    //     // If user is not logged in, visit splash page
+    //     res.sendFile(path.join(__dirname, "../public/login.html"));
 
-    });
+    // });
 
-    // Create a new contact
-    app.post('/new', function(req, res) {
-        // Post new contact details
-    });
+    // // Create a new contact
+    // app.post('/new', function(req, res) {
+    //     // Post new contact details
+    // });
 
-    // Load create new contact page
-    app.get('/new', function(req, res) {
-        // If not logged in, load login page
-        // Else
-        // Load new contact page
-    });
+    // // Load create new contact page
+    // app.get('/new', function(req, res) {
+    //     // If not logged in, load login page
+    //     // Else
+    //     // Load new contact page
+    // });
 
-    // Update your profile info
-    app.post('/profile', function(req, res) {
-        // If not logged in, load login page
-        // Else
-        // Post updated profile details
-    });
+    // // Update your profile info
+    // app.post('/profile', function(req, res) {
+    //     // If not logged in, load login page
+    //     // Else
+    //     // Post updated profile details
+    // });
 
-    // Load profile page
-    app.get('/profile', function(req, res) {
-        // If not logged in, load login page
-        // Else
-        // Load personal profile page
-    });
+    // // Load profile page
+    // app.get('/profile', function(req, res) {
+    //     // If not logged in, load login page
+    //     // Else
+    //     // Load personal profile page
+    // });
 
-    // List of contacts page
-    app.get('/list', function(req, res) {
-        // If not logged in, load login page
-        // Else
-        // Load contact list
-    });
+    // // List of contacts page
+    // app.get('/list', function(req, res) {
+    //     // If not logged in, load login page
+    //     // Else
+    //     // Load contact list
+    // });
 
-    // Results page
-    app.post('/results', function(req, res) {
-        // Load results page
-    });
+    // // Results page
+    // app.post('/results', function(req, res) {
+    //     // Load results page
+    // });
 
-    // Login page
-    app.get('/login', function(req, res) {
-        // Load login page
-    });
+    // // Login page
+    // app.get('/login', function(req, res) {
+    //     // Load login page
+    // });
 
-    // Sign Up page
-    app.get('/signup', function(req, res) {
-        // Load sign up page
-    });
-
-};
+    // // Sign Up page
+    // app.get('/signup', function(req, res) {
+    //     // Load sign up page
+    // });
 
 
-var path = require('path');
-
-module.exports = function (app) {
 
     app.get('/signUp', function(req,res) {
         res.sendFile(path.join(__dirname + '/../public/signUpPage.html'));
@@ -84,8 +79,8 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname + '/../public/profilePage.html'));
     });
 
-    app.use( function(req,res) {
-        res.sendFile(path.join(__dirname + '/../public/splashPage.html'));
+    app.get('/', function(req,res) {
+        res.sendFile(path.join(__dirname + '/../public/splash.html'));
     });
 }
 
